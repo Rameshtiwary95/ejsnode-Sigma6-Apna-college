b458a9e2d4f100710d5dbeb706cf692fa326d030
tiwaryramesh95@gmail.com <tiwary705014@gmail.com>
2025-06-03 13:28:13 -0300

    update file

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ const port = 8080;
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"/views"));
 
+// home Route
 app.get("/", (req, res) => {
 res.render("home.ejs");
 });
 
+// instagram Route
 app.get("/ig/:username", (req, res) => {
-let { username } = req.params;
-res.render("instagram.ejs",{username});
+let{username} = req.params;
+const instaData = require("./data.json");
+const data = instaData[username];
+console.log(data);
+res.render("instagram.ejs",{ data: instaData[username]});
 });
 
 app.get("/hello", (req, res) => {
