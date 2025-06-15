9a483a36be414307d97ac08037667c498f0a26da
tiwaryramesh95@gmail.com <tiwary705014@gmail.com>
2025-06-15 14:25:49 -0300

    add new files

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require("path");
 
 const port = 8080;
 
+app.use(express.static(path.join(__dirname,"/public/js")));
+app.use(express.static(path.join(__dirname,"/public/css")));
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"/views"));
 
@@ -22,7 +24,7 @@ console.log(data);
 if (data) {
 res.render("instagram.ejs",{data});
 } else { 
-    res.send("user not  found")
+    res.render("error.ejs");
 }
 });
 
